7d051b1f615a7127e55dcd6c9fa920f9a80bf53e
Shimon Doodkin <helpmepro1@gmail.com>
2022-01-22 04:32:17 +0200

    make it keep connection

--- proxy.js
+++ proxy.js
@@ -1,23 +1,40 @@
 var net = require('net');
 
-var LOCAL_PORT  = 6512;
-var REMOTE_PORT = 6512;
-var REMOTE_ADDR = "192.168.1.25";
+var LOCAL_PORT  = 6500;
+var REMOTE_PORT = 9000;
+var REMOTE_ADDR = "127.0.0.1";
 
 var server = net.createServer(function (socket) {
+    console.log('  ** CONNECT **');
+    var serviceSocket = new net.Socket();
+    serviceSocket.connect(parseInt(REMOTE_PORT), REMOTE_ADDR);
+    serviceSocket.on("data", function (data) {
+        console.log('>> From proxy to client', JSON.stringify(data.toString()));
+        socket.write(data);
+    });
     socket.on('data', function (msg) {
-        console.log('  ** START **');
         console.log('<< From client to proxy ', msg.toString());
-        var serviceSocket = new net.Socket();
-        serviceSocket.connect(parseInt(REMOTE_PORT), REMOTE_ADDR, function () {
-            console.log('>> From proxy to remote', msg.toString());
-            serviceSocket.write(msg);
-        });
-        serviceSocket.on("data", function (data) {
-            console.log('<< From remote to proxy', data.toString());
-            socket.write(data);
-            console.log('>> From proxy to client', data.toString());
-        });
+        serviceSocket.write(msg);
+    });
+    
+    socket.on('close', function (msg) {
+        console.log('<< From client to proxy close');
+        serviceSocket.destroy();
+    });
+
+    serviceSocket.on('close', function (msg) {
+        console.log('<< From proxy to client close');
+        socket.destroy();
+    });
+
+    socket.on('end', function (msg) {
+        console.log('<< From client to proxy end');
+        serviceSocket.end();
+    });
+
+    serviceSocket.on('end', function (msg) {
+        console.log('<< From proxy to client end');
+        socket.end();
     });
 });
 
